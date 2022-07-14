c33dc314b3aa6f017d746bd7d97a4ea678ff9b04
Richie0003 <106782587+Richie0003@users.noreply.github.com>
2022-07-14 17:49:25 +0000

    Add files via upload

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - entry point
+ *
+ * @s: pointer
+ * @accept: pointer
+ * Return: pointer or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for ( ; *s; s++)
+	{
+		for (i = 0; *(accept + i); i++)
+		{
+			if (*s == *(accept + i))
+				return (s);
+		}
+	}
+	return ('\0');
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - entry point
+ *
+ * @a: pointer
+ * @size: variable integer
+ * Return: nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, n;
+	int pri = 0;
+	int sec = 0;
+
+	n = size * size;
+	for (i = 0; i < n; i += size + 1)
+	{
+		pri += *(a + i);
+	}
+	for (i = size - 1; i < n - 1; i += size - 1)
+	{
+		sec += *(a + i);
+	}
+
+	printf("%d, %d\n", pri, sec);
+}
